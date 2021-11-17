05e5c8ceff2efe300ffc84b949f8dfeda01bd008
HKGx <19597269+HKGx@users.noreply.github.com>
2021-11-17 23:50:13 +0100

    main: delete stringifiy

--- main.py
+++ main.py
@@ -3,85 +3,8 @@ from pprint import pprint
 
 from smol.checker import Checker
 from smol.interpret import Interpreter
-from smol.parser import (AdditionExpression, ArrayExpression,
-                         AssignmentStatement, BlockExpression, BooleanExpression, BreakExpression,
-                         ComparisonExpression, ContinueExpression,
-                         EqualityExpression, ExponentiationExpression,
-                         Expression, ExpressionStatement, ForStatement,
-                         FunctionCallExpression, FunctionDefinitionStatement, IdentifierExpression,
-                         IfExpression, IntegerExpression,
-                         MultiplicationExpression, NegationExpression, Parser,
-                         Program, RangeExpression, Statement, StringExpression, WhileStatement)
+from smol.parser import Parser
 from smol.tokenizer import Tokenizer
-
-
-def program(prog: Program) -> str:
-    return '\n'.join(statement(stmt) for stmt in prog.statements)
-
-
-def statement(stmt: Statement, indent: int = 0) -> str:
-    match stmt:
-        case AssignmentStatement(name, value):
-            return f"{stringify(name, indent)} = {stringify(value)}"
-        case ExpressionStatement(expression):
-            return stringify(expression, indent)
-        case ForStatement(ident, expr, body):
-            return f"for {stringify(ident)} in {stringify(expr)} do \n{stringify(body, indent + 1)}\nend"
-        case WhileStatement(condition, body):
-            return f"while {stringify(condition)} do \n{stringify(body, indent + 1)}\nend"
-        case FunctionDefinitionStatement(name, args, body):
-            return f"fn {stringify(name)}({', '.join(stringify(arg) for arg in args)}):\n{stringify(body, indent + 1)}"
-
-    raise Exception(f"Unexpected statement: {stmt}")
-
-
-def stringify(expression: Expression, indent: int = 0) -> str:
-    # TODO: improve indentation
-    match expression:
-        case IdentifierExpression(value):
-            return "\t" * indent + value
-        case IntegerExpression(value):
-            return "\t" * indent + str(value)
-        case StringExpression(value):
-            return "\t" * indent + f'"{value}"'
-        case BooleanExpression(value):
-            return "\t" * indent + str(value)
-        case AdditionExpression(lhs, sign, rhs):
-            return "\t" * indent + f"({stringify(lhs)} {sign} {stringify(rhs)})"
-        case MultiplicationExpression(lhs, sign, rhs):
-            return "\t" * indent + f"({stringify(lhs)} {sign} {stringify(rhs)})"
-        case NegationExpression(value):
-            return "\t" * indent + f"(-{stringify(value)})"
-        case ExponentiationExpression(lhs, sign, rhs):
-            return "\t" * indent + f"({stringify(lhs)} {sign} {stringify(rhs)})"
-        case FunctionCallExpression(name, args):
-            return "\t" * indent + f"{stringify(name)}({', '.join(stringify(arg) for arg in args)})"
-        case ComparisonExpression(left, sign, right):
-            return "\t" * indent + f"{stringify(left)} {sign} {stringify(right)}"
-        case EqualityExpression(left, sign, right):
-            return "\t" * indent + f"{stringify(left)} {sign} {stringify(right)}"
-        case IfExpression(condition, body, elifs, else_body):
-            main_branch = "\t" * indent + \
-                f"if {stringify(condition)}:\n{stringify(body, indent + 1)}"
-            elif_branches = ("\t" * indent + '\n').join(
-                f"elif {stringify(elif_[0])}:\n{stringify(elif_[1], indent + 1)}" for elif_ in elifs)
-            else_branch = f"else:\n{stringify(else_body, indent + 1)}" if else_body else ''
-            return f"{main_branch}\n{elif_branches}\n{else_branch}"
-        case ArrayExpression(values):
-            return f"[{', '.join(stringify(value) for value in values)}]"
-        case RangeExpression(start, end, step):
-            step_ = f"..{stringify(step)}" if step != 1 else ''
-            return f"{stringify(start)}..{stringify(end)}" + step_
-        case BreakExpression():
-            return "\t" * indent + "break"
-        case ContinueExpression():
-            return "\t" * indent + "continue"
-        case BlockExpression(statements):
-            stmts = ('\n').join(statement(stmt, indent + 1)
-                                for stmt in statements)
-            return "\t" * indent + f"do\n{stmts}\n" + "\t" * indent + "end"
-
-    raise Exception(f"Unexpected expression: {expression}")
 
 
 def compile_file(file: TextIOWrapper, debug: bool = False):
@@ -109,7 +32,6 @@ def interpret_file(file: TextIOWrapper, debug: bool = False, no_checker: bool = 
     prog = Parser(tokens).program()
     if debug:
         pprint(prog)
-    print(program(prog))
     if no_checker:
         interpreter = Interpreter(prog)
         pprint(interpreter.run())
@@ -137,7 +59,6 @@ def repl(debug: bool = False):
         prog = Parser(tokens).program()
         if debug:
             pprint(prog)
-        print(program(prog))
         checker = Checker(prog)
         for error in checker.check():
             print(error)
